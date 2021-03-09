9bfc6b05d87bf22ca648453ed22272b0960291a1
Jesse Livezey <jesse.livezey@gmail.com>
2021-03-08 19:39:25 -0800

    rework tests

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -6,37 +6,28 @@ from process_nwb.fft import (mklrfft, mklirfft, mklfft, mklifft,
 
 
 @pytest.fixture
-def data():
+def X():
     return np.random.randn(18134054, 2)
 
 
-def test_mklrfft_fix(data):
-    """Check that mkl bug still exists. When this test passes, the mkl logic in fft.py can be
+@pytest.mark.parametrize("myfft", [mklrfft, mklirfft, mklfft, mklifft])
+def test_mklrfft_fix(myfft, X):
+    """Check that mkl bug still exists. When this test fails, the mkl logic in fft.py can be
     reassesed."""
-    X = data
     with pytest.raises(ValueError):
-        mklrfft(X, axis=0)
-    with pytest.raises(ValueError):
-        mklirfft(X, axis=0)
-    with pytest.raises(ValueError):
-        mklfft(X, axis=0)
-    with pytest.raises(ValueError):
-        mklifft(X, axis=0)
+        myfft(X, axis=0)
 
 
-def test_rfft_fix(data):
+@pytest.mark.parametrize("myfft", [rfft, irfft, fft, ifft])
+def test_rfft_fix(myfft, X):
     """Check that patched functions work on shape with known problem."""
-    X = data
-    rfft(X, axis=0)
-    irfft(X, axis=0)
-    fft(X, axis=0)
-    ifft(X, axis=0)
+    myfft(X, axis=0)
 
 
-@pytest.mark.parameterize("forw, inv", [(rfft, irfft), (fft, ifft)])
-def test_roundtrip(forw, inv):
+@pytest.mark.parametrize("forwinv", [(rfft, irfft), (fft, ifft)])
+def test_roundtrip(forwinv, X):
     """Test that patched functions are setup correctly.
     """
-    for X in [np.random.randn(18134054, 2), np.random.randn(150, 2)]:
-        assert_allclose(inv(forw(X, axis=0), axis=0), X)
-        assert_allclose(inv(forw(X, axis=0), axis=0), X)
+    forw, inv = forwinv
+    assert_allclose(inv(forw(X, axis=0), axis=0), X)
+    assert_allclose(inv(forw(X[:150], axis=0), axis=0), X[:150])
