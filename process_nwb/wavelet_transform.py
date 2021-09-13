70dc1250157d822721bd838d28e2078770c43f15
Jesse Livezey <jesse.livezey@gmail.com>
2021-09-13 15:08:44 -0700

    wavelet iterative write

--- process_nwb/wavelet_transform.py
+++ process_nwb/wavelet_transform.py
@@ -7,6 +7,7 @@ from process_nwb.utils import (_npads, _smart_pad, _trim,
                                chang_sds)
 
 from pynwb.misc import DecompositionSeries
+from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
 
 
 def gaussian(n_time, rate, center, sd):
@@ -62,7 +63,158 @@ def hamming(n_time, rate, min_freq, max_freq):
     return k
 
 
-def wavelet_transform(X, rate, filters='rat', hg_only=True, X_fft_h=None, npad=0):
+def get_filterbank(filters, n_time, rate, hg_only):
+    """Get the filterbank and parameters.
+
+    Parameters
+    ----------
+    n_time : int
+        Input data time dimension.
+    filters : str (optional)
+        Which type of filters to use. Options are
+        'rat': center frequencies spanning 2-1200 Hz, constant Q, 54 bands
+        'human': center frequencies spanning 4-200 Hz, constant Q, 40 bands
+        'changlab': center frequencies spanning 4-200 Hz, variable Q, 40 bands
+        Note - calculating center frequencies above rate/2 raises a ValueError
+        If filters is a list, it is assumed to already be correctly formatted.
+    rate : float
+        Number of samples per second.
+    hg_only : bool
+        If True, only the amplitudes in the high gamma range [70-150 Hz] is computed.
+
+    Returns
+    -------
+    filters : list of ndarrays
+        List of filters to apply.
+    cfs : ndarray
+        Center frequencies used.
+    sds : ndarray
+        Bandwidths used.
+    """
+    if isinstance(filters, list):
+        return filters, None, None
+    
+    # Calculate center frequencies
+    if filters in ['human', 'changlab']:
+        cfs = log_spaced_cfs(4.0749286538265, 200, 40)
+    elif filters == 'rat':
+        cfs = log_spaced_cfs(2.6308, 1200., 54)
+    else:
+        raise NotImplementedError
+
+    # Subselect high gamma bands
+    if hg_only:
+        idxs = np.logical_and(cfs >= 70., cfs <= 150.)
+        cfs = cfs[idxs]
+
+
+    # Raise exception if sample rate too small
+    if cfs.max() * 2. > np.nextafter(rate, np.inf):  # Allow floating point tolerance
+        string = ('Unable to compute wavelet transform above Nyquist rate ({} Hz).' +
+                  ' Increase your rate ({} Hz) to at least twice your desired maximum' +
+                  'frequency of interest.')
+        raise ValueError(string.format(cfs.max() * 2., np.nextafter(rate, np.inf)))
+
+    # Calculate bandwidths
+    if filters in ['rat', 'human']:
+        sds = const_Q_sds(cfs)
+    elif filters == 'changlab':
+        sds = chang_sds(cfs)
+    else:
+        raise NotImplementedError
+
+    filters = []
+    for cf, sd in zip(cfs, sds):
+        filters.append(gaussian(n_time, rate, cf, sd))
+
+    return filters, cfs, sds
+
+
+class ChannelBandIterator(AbstractDataChunkIterator):
+    """Class for iterative write over channels and bands.
+
+    Parameters
+    ----------
+    X : ndarray (n_time, n_channels)
+        Data array.
+    filters : str (optional)
+        Which type of filters to use. Options are
+        'rat': center frequencies spanning 2-1200 Hz, constant Q, 54 bands
+        'human': center frequencies spanning 4-200 Hz, constant Q, 40 bands
+        'changlab': center frequencies spanning 4-200 Hz, variable Q, 40 bands
+    npad : int
+        Padding to add to beginning and end of timeseries. Default 0.
+    hg_only : bool
+        If True, only the amplitudes in the high gamma range [70-150 Hz] is computed.
+    post_resample_rate : float
+        If not `None`, resample the computed wavelet amplitudes to this rate.
+    """
+    def __init__(self, X, rate, filters='rat', npad=None, hg_only=True, post_resample_rate=None):
+        self.X = X
+        self.rate = rate
+        self.npad = npad
+        self.post_resample_rate = post_resample_rate
+
+        # Need to pad X before predicting chunk and filter shape:
+        self.npads, self.to_removes, _ = _npads(X, npad)
+        self.wavelet_time = X.shape[0] + 2 * npad
+        self.filterbank, self.cfs, self.sds = get_filterbank(
+            filters, self.wavelet_time, self.rate, hg_only
+        )
+        self.resample_time = self.X.shape[0]
+        if post_resample_rate is not None:
+            self.resample_time = int(np.ceil(self.wavelet_time * post_resample_rate / rate))
+
+        self.nch = self.X.shape[1]
+        self.nbands = len(self.filterbank)
+        self._i = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        ch = self._i // self.nbands
+        band = self._i % self.nbands
+        self._i += 1
+
+        if ch >= self.X.shape[1]:
+            raise StopIteration
+
+        if band == 0:
+            self.X_fft_h = None
+
+        X_ch = self.X[:, [ch]]
+        X_ch = _smart_pad(X_ch, self.npads)
+        data, self.X_fft_h, cfs, sds = wavelet_transform(
+            X_ch,
+            self.rate,
+            filters=[self.filterbank[band]],
+            X_fft_h=self.X_fft_h,
+            to_removes=self.to_removes
+        )
+        if self.post_resample_rate is not None:
+            data = resample(data, self.post_resample_rate, self.rate)
+        data = np.squeeze(data)
+        return DataChunk(data=np.abs(data), selection=np.s_[:data.shape[0], ch, band])
+
+    next = __next__
+
+    @property
+    def dtype(self):
+        return self.X.dtype
+
+    @property
+    def maxshape(self):
+        return (None, self.nch, self.nbands)
+
+    def recommended_chunk_shape(self):
+        return (self.resample_time, 1, 1)
+
+    def recommended_data_shape(self):
+        return (self.resample_time, self.nch, self.nbands)
+
+
+def wavelet_transform(X, rate, filters='rat', hg_only=True, X_fft_h=None, npad=0, to_removes=None):
     """Apply a wavelet transform using a prespecified set of filters.
 
     Calculates the center frequencies and bandwidths for the wavelets and applies them along with
@@ -109,37 +261,7 @@ def wavelet_transform(X, rate, filters='rat', hg_only=True, X_fft_h=None, npad=0
         n_time = X_fft_h.shape[0]
     freq = fftfreq(n_time, 1. / rate)
 
-    # Calculate center frequencies
-    if filters in ['human', 'changlab']:
-        cfs = log_spaced_cfs(4.0749286538265, 200, 40)
-    elif filters == 'rat':
-        cfs = log_spaced_cfs(2.6308, 1200., 54)
-    else:
-        raise NotImplementedError
-
-    # Subselect high gamma bands
-    if hg_only:
-        idxs = np.logical_and(cfs >= 70., cfs <= 150.)
-        cfs = cfs[idxs]
-
-    # Raise exception if sample rate too small
-    if cfs.max() * 2. > np.nextafter(rate, np.inf):  # Allow floating point tolerance
-        string = ('Unable to compute wavelet transform above Nyquist rate ({} Hz).' +
-                  ' Increase your rate ({} Hz) to at least twice your desired maximum' +
-                  'frequency of interest.')
-        raise ValueError(string.format(cfs.max() * 2., np.nextafter(rate, np.inf)))
-
-    # Calculate bandwidths
-    if filters in ['rat', 'human']:
-        sds = const_Q_sds(cfs)
-    elif filters == 'changlab':
-        sds = chang_sds(cfs)
-    else:
-        raise NotImplementedError
-
-    filters = []
-    for cf, sd in zip(cfs, sds):
-        filters.append(gaussian(n_time, rate, cf, sd))
+    filters, cfs, sds = get_filterbank(filters, n_time, rate, hg_only)
 
     Xh = np.zeros(X.shape + (len(filters),), dtype=np.complex)
     if X_fft_h is None:
@@ -162,7 +284,7 @@ def wavelet_transform(X, rate, filters='rat', hg_only=True, X_fft_h=None, npad=0
 
 
 def store_wavelet_transform(elec_series, processing, filters='rat', hg_only=True, X_fft_h=None,
-                            abs_only=True, npad=0, post_resample_rate=None):
+                            abs_only=True, npad=0, post_resample_rate=None, chunked=True):
     """Apply a wavelet transform using a prespecified set of filters. Results are stored in the
     NWB file as a `DecompositionSeries`.
 
@@ -201,33 +323,51 @@ def store_wavelet_transform(elec_series, processing, filters='rat', hg_only=True
     """
     X = elec_series.data[:]
     rate = elec_series.rate
-    X_wvlt, _, cfs, sds = wavelet_transform(X, rate, filters=filters, X_fft_h=X_fft_h,
-                                            hg_only=hg_only, npad=npad)
-    amplitude = abs(X_wvlt)
-    if post_resample_rate is not None:
-        amplitude = resample(amplitude, post_resample_rate, rate)
-        rate = post_resample_rate
-    elec_series_wvlt_amp = DecompositionSeries('wvlt_amp_' + elec_series.name,
-                                               abs(X_wvlt),
-                                               metric='amplitude',
-                                               source_timeseries=elec_series,
-                                               starting_time=elec_series.starting_time,
-                                               rate=rate,
-                                               description=('Wavlet: ' +
-                                                            elec_series.description))
-    series = [elec_series_wvlt_amp]
-    if not abs_only:
+    if chunked:
+        if not abs_only:
+            raise NotImplementedError("Phase is not implemented for chunked wavelet transform.")
+        X_wvlt_abs = ChannelBandIterator(X, rate, filters=filters, npad=npad, hg_only=hg_only)
+        cfs = X_wvlt_abs.cfs
+        sds = X_wvlt_abs.sds
+        elec_series_wvlt_amp = DecompositionSeries('wvlt_amp_' + elec_series.name,
+                                                   X_wvlt_abs,
+                                                   metric='amplitude',
+                                                   source_timeseries=elec_series,
+                                                   starting_time=elec_series.starting_time,
+                                                   rate=rate,
+                                                   description=('Wavlet: ' +
+                                                                elec_series.description))
+        series = [elec_series_wvlt_amp]
+        X_wvlt = None # this function still needs to return something 
+    else:
+        X_wvlt, _, cfs, sds = wavelet_transform(X, rate, filters=filters, X_fft_h=X_fft_h,
+                                                hg_only=hg_only, npad=npad)
+        amplitude = abs(X_wvlt)
         if post_resample_rate is not None:
-            raise ValueError('Wavelet phase should not be resampled.')
-        elec_series_wvlt_phase = DecompositionSeries('wvlt_phase_' + elec_series.name,
-                                                     np.angle(X_wvlt),
-                                                     metric='phase',
-                                                     source_timeseries=elec_series,
-                                                     starting_time=elec_series.starting_time,
-                                                     rate=rate,
-                                                     description=('Wavlet: ' +
-                                                                  elec_series.description))
-        series.append(elec_series_wvlt_phase)
+            amplitude = resample(amplitude, post_resample_rate, rate)
+            X_wvlt = amplitude
+            rate = post_resample_rate
+        elec_series_wvlt_amp = DecompositionSeries('wvlt_amp_' + elec_series.name,
+                                                   amplitude,
+                                                   metric='amplitude',
+                                                   source_timeseries=elec_series,
+                                                   starting_time=elec_series.starting_time,
+                                                   rate=rate,
+                                                   description=('Wavlet: ' +
+                                                                elec_series.description))
+        series = [elec_series_wvlt_amp]
+        if not abs_only:
+            if post_resample_rate is not None:
+                raise ValueError('Wavelet phase should not be resampled.')
+            elec_series_wvlt_phase = DecompositionSeries('wvlt_phase_' + elec_series.name,
+                                                         np.angle(X_wvlt),
+                                                         metric='phase',
+                                                         source_timeseries=elec_series,
+                                                         starting_time=elec_series.starting_time,
+                                                         rate=rate,
+                                                         description=('Wavlet: ' +
+                                                                      elec_series.description))
+            series.append(elec_series_wvlt_phase)
 
     for es in series:
         for ii, (cf, sd) in enumerate(zip(cfs, sds)):
