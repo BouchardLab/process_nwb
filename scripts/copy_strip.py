afc151ec5d5f1669bbcf143e3b417c840d1273d9
John Hermiz <jthermiz@gmail.com>
2021-10-04 20:19:24 -0700

    Changed default to not overwrite

--- scripts/copy_strip.py
+++ scripts/copy_strip.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
-import argparse, glob, os
+import argparse, os
 from pynwb import NWBHDF5IO
 
 
 parser = argparse.ArgumentParser(description='Copies nwb and strips away processing module')
 parser.add_argument('src_file', type=str, help='Source nwb file')
-parser.add_argument('--dst_file', type=str, default=None,
-                    help='Destination nwb file. If not specified, then overwrites source file')
+parser.add_argument('dst_file', type=str, help='Destination nwb file.' +
+                    '\nIf file is the same as the source, then it will be overwritten')
 
 args = parser.parse_args()
 
@@ -14,7 +14,7 @@ src_file = args.src_file
 dst_file = args.dst_file
 overwrite_flag = False
 
-if (dst_file is None) or (dst_file == src_file):
+if dst_file == src_file:
     dst_file = ''.join(src_file.split('.')[:-1]) + '_tmp.nwb'
     overwrite_flag = True
 
@@ -23,8 +23,8 @@ with NWBHDF5IO(src_file, mode='a') as src_io:
     src_nwb = src_io.read()
     try:
         src_nwb.processing.pop('preprocessing')
-    except:
-        print('No preprocessing module exisits')
+    except KeyError:
+        print('No preprocessing module exists')
 
     with NWBHDF5IO(dst_file, mode='w') as dst_io:
         dst_io.export(src_io=src_io, nwbfile=src_nwb)
